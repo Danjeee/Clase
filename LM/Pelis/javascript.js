73e8ebc96b8de5d991fd409da340f961cd1514e1
Danjeee <javisores@gmail.com>
2024-03-14 21:53:10 +0100

    Subido LM

---
+++ LM/Pelis/javascript.js
@@ -0,0 +1,12 @@
+function chbg1() {
+    document.getElementById('a').style.backgroundImage = "url(img/fondoshrek.jpg)";
+}   
+function chbg2() {
+    document.getElementById('a').style.backgroundImage = "url(img/spaceballsfondo.jpg)";
+}  
+function chbg3() {
+    document.getElementById('a').style.backgroundImage = "url(img/bocadillofondo.jpg)";
+}  
+function chbgdef() {
+    document.getElementById('a').style.backgroundImage = "url()";
+}  
