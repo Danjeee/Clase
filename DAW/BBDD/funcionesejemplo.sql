1a16203f04b3ba2c5f7da52d4514a93e3c72c264
Veronica <javisores@gmail.com>
2024-04-09 10:44:03 +0200

    BBDD funciones

---
+++ DAW/BBDD/funcionesejemplo.sql
@@ -0,0 +1,68 @@
+delimiter $$
+drop procedure if exists hola$$
+create procedure hola()
+begin
+	select "¡Hola mundo!";
+end
+$$
+delimiter ;
+call hola();
+delimiter $$
+drop procedure if exists num$$
+create procedure num(in i INTEGER, out a varchar(50))
+begin
+	declare resultado VARCHAR(50);
+	if i=0 then
+		set resultado = "Cero";
+	elseif i>0 then
+		set resultado = "Positivo";
+	else set resultado = "Negativo";
+    end if;
+    set a = resultado;
+end
+$$
+delimiter ;
+call num(-2,@res);
+select @res;
+delimiter $$
+drop procedure if exists nota1$$
+create procedure nota1(in i INTEGER, out a varchar(50))
+begin
+	declare resultado VARCHAR(50);
+	if i>0 and i<5 then
+		set resultado = "Insuficiente";
+	elseif i>=5 and i<5 then
+		set resultado = "Aprobado";
+	elseif i>=6 and i<7 then
+		set resultado = "Bien";
+	elseif i>=7 and i<9 then
+		set resultado = "Notable";
+	elseif i>=9 then
+		set resultado = "Sobresaliente";
+	else set resultado = "Error";
+    end if;
+    set a = resultado;
+end
+$$
+delimiter ;
+call nota1(8,@res);
+select @res;
+delimiter $$
+drop procedure if exists nota2$$
+create procedure nota2(in i INTEGER, out a varchar(50))
+begin
+	declare resultado VARCHAR(50);
+	case
+		when i >= 0 and i < 5 then set resultado = "Insuficiente";
+        when i >= 5 and i < 6 then set resultado = "Aprobado";
+        when i >=6 and i<7 then set resultado ="Bien";
+        when i >=7 and i<9 then set resultado="Notable";
+        when i>=9 then set resultado = "Sobresaliente";
+        else set resultado = "Error";
+	end case;
+    set a = resultado;
+end
+$$
+delimiter ;
+call nota2(8,@res);
+select @res;
