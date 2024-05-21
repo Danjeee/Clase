741163c6727acc1374028b0670a5ee03920bb322
Veronica <163106867+Danjeee@users.noreply.github.com>
2024-05-21 08:41:41 +0200

    solucionao

--- DAW/LM/Munchkin/munchkin.js
+++ DAW/LM/Munchkin/munchkin.js
@@ -1,39 +1,58 @@
-var vidas = [1]
-function addplayer(){
+var vidas = []
+function addplayer() {
     vidas.push(1);
     console.log(vidas);
     var playercont = document.getElementById("playercont");
     playercont.innerHTML = "";
     reload()
 }
-function reload(){
-    for (var i = 0; i < vidas.length; i++){
+function reload() {
+    for (var i = 0; i < vidas.length; i++) {
         createplayer(i, vidas[i])
     }
 }
-function createplayer(id, lives){
-    console.log("id: " +id)
-    console.log("vidas: " +lives)
-    const newPlayer = document.createElement("player" +id);
+function createplayer(id, lives) {
+    console.log("id: " + id)
+    console.log("vidas: " + lives)
+    const newPlayer = document.createElement("player" + id);
     newPlayer.style.display = "flex";
     const add = document.createElement("add");
     add.id = "add";
-    add.onclick=function addlvl(){addlvl(id)};
+    add.onclick = function () { addlvl(id); };
     console.log(add.onclick)
     const vidas = document.createTextNode(lives);
     const rest = document.createElement("rest");
     rest.id = "rest";
+    rest.onclick = function () { rmlvl(id); };
     newPlayer.appendChild(add)
     newPlayer.appendChild(vidas)
     newPlayer.appendChild(rest)
     var playercont = document.getElementById("playercont");
-    playercont.appendChild(newPlayer);    
+    playercont.appendChild(newPlayer);
 }
-function addlvl(){
-    for (var i = 0; i < vidas.length; i++){
-        if (id == i){
-            vidas[i]++
+function addlvl(id) {
+    for (var i = 0; i < vidas.length; i++) {
+        if (id == i) {
+            if (vidas[i] != 10) {
+                vidas[i]++
+            }
+            var playercont = document.getElementById("playercont");
+            playercont.innerHTML = "";
             reload()
+            break
+        }
+    }
+}
+function rmlvl(id) {
+    for (var i = 0; i < vidas.length; i++) {
+        if (id == i) {
+            if (vidas[i] != 1) {
+                vidas[i]--
+            }
+            var playercont = document.getElementById("playercont");
+            playercont.innerHTML = "";
+            reload()
+            break
         }
     }
 }
