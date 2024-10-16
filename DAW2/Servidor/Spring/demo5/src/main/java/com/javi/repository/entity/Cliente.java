498ab75a561f371b041b58b2b1a0fbe1fd282a2a
Danjeee <javisores@gmail.com>
2024-10-16 13:57:38 +0200

    dsf

---
+++ DAW2/Servidor/Spring/demo5/src/main/java/com/javi/repository/entity/Cliente.java
@@ -0,0 +1,49 @@
+package com.javi.repository.entity;
+
+public class Cliente {
+	private int id;
+	private String nif;
+	private String nombre;
+	private String apellidos;
+	private String clave;
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNif() {
+		return nif;
+	}
+	public void setNif(String nif) {
+		this.nif = nif;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Cliente() {
+		super();
+	}
+}
