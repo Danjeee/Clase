3034ab70480d762875f292b76058c52059c1cd1e
Danjeee <javisores@gmail.com>
2024-10-21 10:58:36 +0200

    as

--- DAW2/Cliente/dwc/ejemploArticulos/js.js
+++ DAW2/Cliente/dwc/ejemploArticulos/js.js
@@ -1,8 +1,9 @@
 'use strict'
 
-let xmlfinal = document.createElement('div')
+let xmlfinal = document.getElementById('final')
 let xmlcont = "<table><tr><th>Id</th><th>Familia</th><th>Nombre</th><th>Proveedor</th><th>Precio</th></tr>"
 var selectcont = document.getElementById('selectcont')
+let selectcontbase = selectcont.innerHTML
 var familias
 var proveedores
 
@@ -15,87 +16,112 @@ function procesarArticulos(opc, fam, prov) {
     if (prov != null) {
         parameters += "&proveedor=" + prov
     }
-    p.open('GET', "./consultarArticulos.php?"+parameters)
-    p.addEventListener('load', function(){
-        if (p.status == 200){
-            console.log(parameters)
-            console.log(p.responseURL)
-            console.log(p.responseText)
+    p.open('GET', "./consultarArticulos.php?" + parameters)
+    p.addEventListener('load', function () {
+        if (p.status == 200) {
             mostrarArticulos(p.responseText)
         }
     })
-    p.send(parameters)
+    p.send()
 }
 function loadFamilias() {
     let p = new XMLHttpRequest()
     let parameters = "opcion=TF"
-    p.open('GET', "./consultarArticulos.php?"+parameters)
-    p.addEventListener('load', function(){
-        if (p.status == 200){
+    p.open('GET', "./consultarArticulos.php?" + parameters)
+    p.addEventListener('load', function () {
+        if (p.status == 200) {
             familias = JSON.parse(p.responseText)
         }
     })
-    p.send(parameters)
+    p.send()
 }
 function loadProv() {
     let p = new XMLHttpRequest()
     let parameters = "opcion=TP"
-    p.open('GET', "./consultarArticulos.php?"+parameters)
-    p.addEventListener('load', function(){
-        if (p.status == 200){
+    p.open('GET', "./consultarArticulos.php?" + parameters)
+    p.addEventListener('load', function () {
+        if (p.status == 200) {
             proveedores = JSON.parse(p.responseText)
         }
     })
-    p.send(parameters)
+    p.send()
 }
 function mostrarArticulos(resp) {
-    var data = JSON.parse(resp)
-    data.forEach(element => {
-        xmlcont += "<tr>"
-        xmlcont += "<td>" + element.idarticulo + "</td>"
-        xmlcont += "<td>" + element.familia + "</td>"
-        xmlcont += "<td>" + element.descripcion + "</td>"
-        xmlcont += "<td>" + element.proveedor + "</td>"
-        xmlcont += "<td>" + element.precioventa + "</td>"
-        xmlcont += "</tr>"
-    });
-    xmlcont += "</table>"
-    xmlfinal.innerHTML = xmlcont
-    document.body.appendChild(xmlfinal)
-}
-function doaction(){
+    try {
+        xmlcont = "<table><tr><th>Id</th><th>Familia</th><th>Nombre</th><th>Proveedor</th><th>Precio</th></tr>"
+        var data = JSON.parse(resp)
+        data.forEach(element => {
+            xmlcont += "<tr>"
+            xmlcont += "<td>" + element.idarticulo + "</td>"
+            xmlcont += "<td>" + element.familia + "</td>"
+            xmlcont += "<td>" + element.descripcion + "</td>"
+            xmlcont += "<td>" + element.proveedor + "</td>"
+            xmlcont += "<td>" + element.precioventa + "</td>"
+            xmlcont += "</tr>"
+        });
+        xmlcont += "</table>"
+        xmlfinal.innerHTML = xmlcont
+    } catch (error) {
+        xmlfinal.innerHTML = "<h1>Lo sentimos, no hay articulos en esta categoria</h1>"
+
+    }
     
+}
+function doaction() {
+
     switch (document.getElementById("opc1").value) {
         case "TA":
-            
-            procesarArticulos("TA",null,null)
-            
+            xmlfinal.innerHTML = ""
+            selectcont.innerHTML = selectcontbase
+            procesarArticulos("TA", null, null)
+
             break;
         case "TF":
             selectFamilia()
             break;
-    
-        default:
+
+        case "TP":
+            selectProveedor()
             break;
     }
 }
-function selectFamilia(){
-    if (document.getElementById('opc2') != null){
-        document.removeChild('opc2')
+function selectFamilia() {
+     xmlfinal.innerHTML = ""
+    if (document.getElementById('opc2') != null) {
+        selectcont.innerHTML = selectcontbase
     }
     let s = document.createElement('select')
     s.id = "opc2"
     s.innerHTML = "<option value='Selecciona una familia' selected hidden>Selecciona una familia</option>"
     Array.from(familias).forEach(e => {
-        s.innerHTML += "<option value='"+e.nombre+"'>"+e.nombre+"</option>"
+        s.innerHTML += "<option value='" + e.nombre + "'>" + e.nombre + "</option>"
     });
-    
+
     selectcont.appendChild(s)
-    s.addEventListener('change', function(){
-        if (s.value != 'Selecciona una familia'){
-            console.log(s.value)
+    s.addEventListener('change', function () {
+        if (s.value != 'Selecciona una familia') {
             procesarArticulos("AF", s.value, null)
         }
     })
 }
+function selectProveedor() {
+     xmlfinal.innerHTML = ""
+    if (document.getElementById('opc2') != null) {
+        selectcont.innerHTML = selectcontbase
+    }
+    let s = document.createElement('select')
+    s.id = "opc2"
+    s.innerHTML = "<option value='Selecciona un proveedor' selected hidden>Selecciona un proveedor</option>"
+    Array.from(proveedores).forEach(e => {
+        s.innerHTML += "<option value='" + e.nombre + "'>" + e.nombre + "</option>"
+    });
+
+    selectcont.appendChild(s)
+    s.addEventListener('change', function () {
+        if (s.value != 'Selecciona un proveedor') {
+            procesarArticulos("AP", null, s.value)
+        }
+    })
+}
+loadProv()
 loadFamilias()
