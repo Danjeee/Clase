e4d8319d40518c0f0032d431a945222dd0747d59
ocar <javisores@gmail.com>
2024-11-22 11:59:08 +0100

    angula

---
+++ DAW2/Cliente/Angular/proyecto001/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  //Ej1
+  title = 'proyecto001';
+
+  //EJ2
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio = 'http://www.google.com';
+
+  //EJ3
+  contador = 1;
+
+  //EJ2
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma=0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }
+
+  //Ej3
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
+}
