0eb7f9dba6b4d1c12e887e696fe9d389fad9a6bc
Danjeee <163106867+Danjeee@users.noreply.github.com>
2025-01-15 09:45:25 +0100

    Servidor ud 5.1

---
+++ DAW2/Servidor/Spring/dws-u5-javiersoriano/src/main/java/com/javi/DwsU5JaviersorianoApplication.java
@@ -0,0 +1,13 @@
+package com.javi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsU5JaviersorianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwsU5JaviersorianoApplication.class, args);
+	}
+
+}
